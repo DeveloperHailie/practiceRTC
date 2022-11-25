e8bd8b3758c48b7c3dec65f00e081c3b5590ea87
hailie <59546976+DeveloperHailie@users.noreply.github.com>
2022-11-25 16:13:00 +0900

    feat: 1:N P2P
    - 1:1 P2P 에서 sender, receiver 추가

--- 1vsN_P2P/server.js
+++ 1vsN_P2P/server.js
@@ -6,12 +6,13 @@ const io = require("socket.io")(server);
 app.use("/", express.static("public"));
 
 const maximum = 4;
+let roomMember = {};
 
 io.on("connection", (socket) => {
   socket.on("join", (roomId) => {
-    console.log(socket.adapter.rooms);
-    console.log(socket);
-    console.log(io.adapter);
+    //console.log(socket.adapter.rooms);
+    //console.log(socket);
+    //console.log(io.adapter);
 
     console.log(`${socket.id}가 서버로 join(roomId) 보냄`);
 
@@ -22,47 +23,51 @@ io.on("connection", (socket) => {
     //console.log("rooms",io.sockets.adapter.rooms[roomId], roomClients, numberOfClients);
 
     // These events are emitted only to the sender socket.
+    let socketMessage = "room_joined";
     if (numberOfClients == 0) {
       console.log(`서버가 ${socket.id}로 roomCreated(roomId) 보냄`);
       socket.join(roomId);
-      socket.emit("room_created", roomId);
-    } else if (numberOfClients <= maximum) {
+      socketMessage = "room_created";
+    } else if (numberOfClients < maximum) {
       console.log(`서버가 ${socket.id}로 roomJoined(roomId) 보냄`);
       socket.join(roomId);
-      socket.emit("room_joined", roomId);
     } else {
       console.log(`서버가 ${socket.id}로 fullRoom(roomId) 보냄`);
       socket.emit("full_room", roomId);
+      return;
     }
+
+    memberSockets = [];
+    for ( memberSocket in socket.adapter.rooms[roomId]){
+      memberSockets.push(memberSocket);
+    }
+    roomMember[roomId]=memberSockets;
+    
+    socket.emit(socketMessage, {roomId, memberSockets});
+    socket.broadcast.to(roomId).emit("member_joined", {memberSockets, sender:socket.id}); //자기 자신 제외, 같은 방 다른 멤버들에게 member_joined
+  });
+
+  // offer : join한 member에게 offer를 보냄 (자신의 RTCSeccionDescription)
+  socket.on("webrtc_offer", (event) => {
+    console.log(`${socket.id}가 서버로 offer(event) 보냄`);
+
+    io.sockets.socket(event.receiver).send("webrtc_offer", {sender:socket.id ,sdp:event.sdp});
     console.log('\n');
   });
 
-  // These events are emitted to all the sockets connected to the same room except the sender.
-  socket.on("start_call", (roomId) => {
-    console.log(`${socket.id}가 서버로 startCall(roomId) 보냄`);
-    console.log(`서버가 전송자랑 같은방에 있는 애한테 startCall(roomId) 보냄`);
-    socket.broadcast.to(roomId).emit("start_call");
-    console.log('\n');
-  });
-  socket.on("webrtc_offer", (event) => {
-    console.log(`${socket.id}가 서버로 offer(event) 보냄`);
-    console.log("받은값",event);
-    console.log(`서버가 전송자랑 같은방에 있는 애한테 offer(event.sdp) 보냄`);
-    socket.broadcast.to(event.roomId).emit("webrtc_offer", {sdp:event.sdp, sendID:socket.id});
-    console.log('\n');
-  });
+  // answer : offer를 보낸 user에게 answer를 보냄 (자신의 RTCSessionDescription)
   socket.on("webrtc_answer", (event) => {
     console.log(`${socket.id}가 서버로 answer(event) 보냄`);
-    console.log("받은값",event);
-    console.log(`서버가 전송자랑 같은방에 있는 애한테 answer(event.sdp) 보냄`);
-    socket.broadcast.to(event.roomId).emit("webrtc_answer", {sdp:event.sdp, sendID:socket.id});
+
+    io.sockets.socket(event.receiver).send("webrtc_answer", {sender:socket.id ,sdp:event.sdp});
     console.log('\n');
   });
+
+  // candidate : 자신의 ICECandidate 정보를 signal(offer 또는 answer)을 주고 받은 상대에게 전송
   socket.on("webrtc_ice_candidate", (event) => {
     console.log(`${socket.id}가 서버로 iceCandidate(event) 보냄`);
-    console.log("받은값",event);
-    console.log(`서버가 전송자랑 같은방에 있는 애한테 iceCandidate(event) 보냄`);
-    socket.broadcast.to(event.roomId).emit("webrtc_ice_candidate",  {event, sendID:socket.id});
+
+    io.sockets.socket(event.receiver).send("webrtc_ice_candidate", {sender:socket.id, candidate: event.candidate});
     console.log('\n');
   });
 });
